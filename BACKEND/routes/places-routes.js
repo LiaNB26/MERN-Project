8d908ab5330e4dd743329258c27532b019c9a83d
Lia <liabrachio@gmail.com>
2020-11-11 15:15:17 +0200

    starting backend

---
+++ BACKEND/routes/places-routes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.history.com/.image/t_share/MTU3ODc3NjU2NzUxNTgwODk1/this-day-in-history-05011931---empire-state-building-dedicated.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building1",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.history.com/.image/t_share/MTU3ODc3NjU2NzUxNTgwODk1/this-day-in-history-05011931---empire-state-building-dedicated.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u2",
+  },
+];
+
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  res.json({ place: place });
+});
+
+module.exports = router;
